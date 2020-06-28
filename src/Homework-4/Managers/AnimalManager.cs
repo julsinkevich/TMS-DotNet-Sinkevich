ec15957e755ff2cc6497aec2bec997c0483c633e
julsinkevich <julsinkevich@users.noreply.github.com>
2020-06-28 23:29:27 +0300

    init: introduction to architecture

---
+++ src/Homework-4/Managers/AnimalManager.cs
@@ -0,0 +1,58 @@
+﻿using hw4.Enums;
+using hw4.Interfaces;
+using hw4.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw4.Managers
+{
+    class AnimalManager: IAnimalManager
+    {
+        public void GetInfo(Animal animal)
+        {
+            Console.WriteLine($"ID:{animal.GetId()}");
+            Console.WriteLine($"Name:{animal.Name}");
+            Console.WriteLine($"Kind:{animal.Kind}");
+            Console.WriteLine($"Habitat:{animal.Habitat}");
+        }
+
+        public void Rename(Animal animal, string name)
+        {
+            animal.Name = name;
+        }
+
+
+        public Animal CreateAnimal()
+        {
+            return new Animal();
+        }
+        public Animal CreateAnimal(string name)
+        {
+            return new Animal(name);
+        }
+        public Animal CreateAnimal(string name, KindType kind)
+        {
+            return new Animal(name, kind);
+        }
+        public Animal CreateAnimal(string name, string habitat)
+        {
+            return new Animal(name, KindType.None, SetHabitat(habitat));
+        }
+
+        public Animal CreateAnimal(string name, KindType kind, string habitat)
+        {
+            return new Animal(name, kind, SetHabitat(habitat));
+        }
+
+        public Habitat SetHabitat(string habitat)
+        {
+            
+            if (Enum.TryParse(habitat, true, out Habitat habitatEnum))
+            {
+                return habitatEnum;
+            }
+            return Habitat.None;
+        }
+    }
+}
